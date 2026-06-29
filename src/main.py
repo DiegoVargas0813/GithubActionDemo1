d1bfd3e9bdc40cd5c5b404bfb1724377d0a3d012
DiegoVargas0813 <diegoenrique.vr@gmail.com>
2026-06-29 12:11:23 -0600

    Correct ruff check

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os # Codigo deliberadamente erroneo con un import no utilizado e implementacion incorrecta de la calculadora para testeo.
+#import os # Codigo deliberadamente erroneo con un import no utilizado e implementacion incorrecta de la calculadora para testeo.
 
 class Calculator:
     def sum(self, a: int, b:int) -> int:
