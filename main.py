031f21d04335c79597ded0342d9f554cca621303
DiegoVargas0813 <diegoenrique.vr@gmail.com>
2026-06-29 11:58:51 -0600

    Add workflow

--- main.py
+++
@@ -1,5 +0,0 @@
-import os # Codigo deliberadamente erroneo con un import no utilizado e implementacion incorrecta de la calculadora para testeo.
-
-class Calculator:
-    def sum(self, a: int, b:int) -> int:
-        return 0
--- tests.py
+++
@@ -1,5 +0,0 @@
-from main import Calculator
-
-# El test intentara sumar 2 + 2
-def test_sums_2_numbers():
-    assert Calculator().sum(2,2) == 4
