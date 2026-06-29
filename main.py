36782d012da7f4103e785ffd6fe56ec15f93578c
DiegoVargas0813 <diegoenrique.vr@gmail.com>
2026-06-29 11:40:18 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import os # Codigo deliberadamente erroneo con un import no utilizado e implementacion incorrecta de la calculadora para testeo.
+
+class Calculator:
+    def sum(self, a: int, b:int) -> int:
+        return 0
